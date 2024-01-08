6a19cc4a1aa8f676c49079a0a30afc01448996f1
BortoletoYan <Yan.Bortoleto@Cevalogistics.com>
2024-01-08 16:29:54 -0300

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'; // Corrigindo o nome do arquivo de estilo
+import axios from 'axios'
 
 const Login = () => {
+    const [values,SetValues] = useState ({
+        id:'',
+        password:''
+    });
+
+    const handleSubmit = (event)=>{
+        event.preventDefault()
+        axios.post('http://localhost:3001/auth/adminlogin')
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+
     return (
+        
         <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
             <div className='p-3 rounded w-25 border loginForm'>
                 <h2>Login</h2>
@@ -10,12 +24,12 @@ const Login = () => {
                     <div className='mb-3'>
                         <label htmlFor='id'><strong>ID:</strong></label>
                         <input type='text' name='id_employee' autoComplete='off' placeholder='Digite o Id'
-                            className='form-control rounded-0' />
+                           onChange={(e)=> SetValues({...values,id: e.target.value})} className='form-control rounded-0' />
                     </div>
                     <div className='mb-3'>
                         <label htmlFor='password'><strong>Password:</strong></label>
                         <input type='text' name='password' autoComplete='off' placeholder='Digite sua senha'
-                            className='form-control rounded-0' />
+                           onChange={(e)=> SetValues({...values,password: e.target.value})} className='form-control rounded-0' />
                     </div>
                     <button className='btn btn-success w-100 rounded-0 mt-3'>Enviar</button>
                     <div className='mb-1'>
