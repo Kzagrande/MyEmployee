0d7a66f69edc080bc3d2f7a8826d5c8d19e5aaad
BortoletoYan <138122033+BortoletoYan@users.noreply.github.com>
2024-01-10 13:17:33 -0300

    feat: dev-sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './components/Login'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Dashboard from './components/Dashboard'
+import Home from './components/Home'
+import AddEmployee from './components/AddEmployee'
+import ListEmployee from './components/ListEmployee'
+import RestrictEmployee from './components/RestrictEmployee'
+import Settings from './components/Settings'
 
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='adminlogin' element={<Login/>}></Route>
-      <Route path='dashboard' element={<Dashboard/>}></Route>
+      <Route path='/adminlogin' element={<Login/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}>
+      <Route path='' element={<Home/>}></Route>
+      <Route path='/dashboard/addemployee' element={<AddEmployee/>}></Route>
+      <Route path='/dashboard/listemployee' element={<ListEmployee/>}></Route>
+      <Route path='/dashboard/restrictlist' element={<RestrictEmployee/>}></Route>
+      <Route path='/dashboard/settings' element={<Settings/>}></Route>
+
+      </Route>
+      
     </Routes>
     </BrowserRouter>
 
---
+++ frontend/src/components/RestrictEmployee.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RestrictEmployee() {
+  return (
+    <div>RestrictEmployee</div>
+  )
+}
+
+export default RestrictEmployee
