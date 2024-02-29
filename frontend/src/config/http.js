972fc5934cb4a920dd9a74667ad98293da0fa929
BortoletoYan <138122033+BortoletoYan@users.noreply.github.com>
2024-02-29 18:52:54 -0300

    feat: update-env

--- frontend/src/config/http.js
+++ frontend/src/config/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const http = axios.create({baseURL:'http://localhost:3001'})
+const http = axios.create({baseURL:'https://myemployee.com.br/api'})
 http.defaults.withCredentials = true;
 
 export default http
